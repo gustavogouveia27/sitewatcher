ab4473ee23caad2e0c0e6036a0f6a6bcadcb80d6
Gouveia <gustavogouveiasnts@gmail.com>
2025-12-18 12:52:04 -0300

    Gouveia

---
+++ app/navbar.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+
+    <div className=" ">
+      {/*<div className="text-center font-medium py-2 bg-gradient-to-r from-violet-500 via-[#9938CA] to-[#E0724A]">*/}
+      <div className="opacity-0 ">
+        <p>Cupom de primeira <span className="underline underline-offset-2">compra!</span></p>
+      </div>
+
+      <nav className="opacity-90 relative h-[70px] flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-8 py-4 bg-white text-gray-900 transition-all shadow mt-6 mx-6 md:mx-16 lg:mx-24 xl:mx-32 rounded-full">
+        <a>
+          <figure className="flex items-center">
+            <Image
+              src="/logo.png"
+              alt="Tênis de corrida vermelho"
+              width={120}
+              height={100}
+              className="position absolute"
+            />
+            <figcaption className="ml-25 font-bold text-2xl font-mono bg-gradient-to-r from-violet-500 via-[#9938CA] to-[#E0724A] bg-clip-text text-transparent">
+              Watcher
+            </figcaption>
+          </figure>
+        </a>
+
+        <ul className="hidden md:flex items-center space-x-8 md:pl-28">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Pricing</a></li>
+        </ul>
+
+
+        <button aria-label="menu-btn" type="button" className="menu-btn inline-block md:hidden active:scale-90 transition">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+            <path d="M3 7a1 1 0 1 0 0 2h24a1 1 0 1 0 0-2zm0 7a1 1 0 1 0 0 2h24a1 1 0 1 0 0-2zm0 7a1 1 0 1 0 0 2h24a1 1 0 1 0 0-2z" />
+          </svg>
+        </button>
+
+        <div className="mobile-menu absolute top-[70px] left-0 w-full bg-white shadow-sm p-6 hidden md:hidden">
+          <ul className="flex flex-col space-y-4 text-lg">
+            <li><a href="#" className="text-sm">Home</a></li>
+            <li><a href="#" className="text-sm">Services</a></li>
+            <li><a href="#" className="text-sm">Portfolio</a></li>
+            <li><a href="#" className="text-sm">Pricing</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,53 @@
 import Image from "next/image";
+import Navbar from "./navbar";
+import Show from "./show";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="relative min-h-screen overflow-hidden">
+      <Navbar />
+
+      {/* BACKGROUND IMAGE */}
+      <Image
+        src="/background.png"
+        alt="Background"
+        fill
+        priority
+        className="object-cover absolute inset-0 -z-10 opacity-90"
+      />
+
+      <Image
+        src="/unnmaed.png"
+        alt="unnmaed"
+        width={1100}
+        height={500}
+        priority
+        className="position absolute ml-170"
+      />
+
+      {/* Overlay escuro (opcional, mas recomendado) */}
+      <div className="absolute inset-0 bg-black/40 -z-10"></div>
+
+      {/* CONTEÚDO */}
+      <div className="relative z-10 mt-40 ml-40 max-w-xl">
+        <h1 className="text-7xl font-extrabold text-white leading-tight">
+          O mais seguro e <span className="text-[#aa00ff]">MELHOR</span><br/> cheat de CS2
+          do mercado!
+        </h1>
+
+        <h2 className="mt-8 text-[#858585]">
+          Cansado de ficar por último na tabela e depender do seu time?
+        </h2>
+
+        <button className="mt-8 bg-[#83348b] hover:bg-[#9b4bd1] text-white font-bold py-3 px-8 rounded-lg text-lg transition shadow-[0_0_20px_#83348b]">
+          COMPRE AGORA!
+        </button>
+      </div>
+    </main>
   );
 }
+        
+
+    
+    
+    
